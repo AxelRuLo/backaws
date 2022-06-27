06308123d4d0c8172d5e98087bc0e73921cdd342
Axel Ruiz L <developeraxrulo@gmail.com>
2022-06-27 12:11:57 -0500

    backaws

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: '172.17.0.1',
+//     user: 'root',
+//     password: 'root',
+//     database: 'vetcitas',
+//     port: 3306
+// });
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    return res.status(200).send("todo ok")
+})
+app.get("/prueba", (req, res) => {
+    return res.status(200).send("esto debe de ser la llamada a api")
+})
+
+
+app.listen(port, function () {
+    console.log(`servicio 4 funcionando en el puerto ${port}`)
+    // connection.connect(function (error) {
+        // if (error) {
+            // throw error;
+        // } else {
+            // console.log('Conexion correcta.');
+        // }
+    // });
+});
